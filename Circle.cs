9b23ddaf641eb8621cf3322997ce49724a211d23
EvanSimon <evansimon07@gmail.com>
2018-10-23 20:51:02 -0400

    This is lab10 you can tell its lab10 because of the way it is

---
+++ Circle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLab10
+{
+
+    class Circle
+    {
+        public const double PI = Math.PI;
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                radius = value;
+            }
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+
+        }
+
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+
+        public double CaluculateCircumference()
+        {
+            double circumference = PI * (radius * 2);
+            return circumference;
+        }
+
+        public string CaluculateFormattedCircumference()
+        {
+
+            return FormatNumber(CaluculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            double area = PI * (radius * radius);
+            return area;
+        }
+
+        public string CalculatedFormatedArea()
+        {
+            return FormatNumber(CalculateArea());
+
+        }
+
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            bool response = true;
+            while (response == true)
+            {
+
+                Console.WriteLine("Welcome to the Circle Tester");
+
+                Console.WriteLine("Enter radius");
+                double UserInput = double.Parse(Console.ReadLine());
+
+                Circle myCircle = new Circle(UserInput);
+
+                Console.WriteLine($"Circumfrance: {myCircle.CaluculateCircumference()}");
+                Console.WriteLine($"Circumfrance:{myCircle.CaluculateFormattedCircumference()}");
+
+                Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+                Console.WriteLine($"Area: {myCircle.CalculatedFormatedArea()}");
+              
+                i++;
+                Console.WriteLine($"Circles:{i}");
+
+                response = GetYorN();
+
+
+            }
+
+            
+        }
+
+        public static bool GetYorN()
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("Would you like to continue y/n ?");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y") //if yes it breaks this loops and the false function runs it back to the top of the program loop
+                {
+                  
+                    break;
+                }
+
+                else if (answer == "n")//if no it breaks the loop agian but true function ends the program; n
+                {
+
+                   return false;
+                }
+
+
+                else if (answer != "y" || answer != "n") //if anything other then y or n it continues to ask the user
+                {
+                    
+                    continue;
+                }
+
+            }
+
+            return true;
+        }
+
+    }
+}
